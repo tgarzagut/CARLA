f4e51a09c4e1535f10e0dcf4cc231e80597a047c
tgarzagut <102454563+tgarzagut@users.noreply.github.com>
2024-02-23 20:15:36 +0000

    to be added from local

---
+++ Tests/controller_outside_test.py
@@ -0,0 +1,7 @@
+"""
+    Code already exists but, using the pygame library, this code (to be inserted) tests and prints the state of 
+    each button, wheel, pedal, etc for the Logitech G920 controller.
+"""
+import pygame
+import sys
+
